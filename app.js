072800d52c4791e04e5ce554c1a56eb37dd29a8b
cliffgor <clierozigy@gmail.com>
2020-02-12 15:09:23 +0300

    added a grpahql request parameter to diplay the list of events

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello Booked");
-});
+// Qraphql requests
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(
+      `
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float
+            date: String!
+        }
+
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    
+    
+        `
+    ),
+    rootValue: {
+      events: () => {
+        return ["Welcome to React course work"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 app.listen(3000);
